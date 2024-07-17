e6d5e84e940e6c0a85aa73603181e8743970e44a
Jeremy <encaladajeremy@gmail.com>
2024-07-17 09:26:50 -0500

    feat: Add projects data to projects.js

---
+++ src/utils/projects.js
@@ -0,0 +1,11 @@
+export const projects = [
+  {
+    id: 1,
+    name: 'Project 1',
+    description: 'Description 1',
+    image: 'https://via.placeholder.com/150',
+    demo: 'https://www.google.com',
+    code: 'https://www.google.com'
+
+  }
+]
